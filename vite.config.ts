92316b567cec3c8ba7935a254859c6109d9241ff
kirabelll <kirubelkassahun9@gmail.com>
2026-08-31 15:04:21 +0300

    chore: update API proxy target address to 127.0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true }
+      '/api': { target: 'http://127.0.0.1:5000', changeOrigin: true }
     }
   }
 });
